70cc125a26ba4f5285bfd978f19a60f3d6bc25fb
Samuel-Sorial <eng.samuel.maged@gmail.com>
2021-03-12 23:49:48 +0200

    Allowing execution to continue if corrupted xml

--- parser.py
+++ parser.py
@@ -7,7 +7,12 @@ def main():
     format, files = extract_args()
     validate_args(format, files)
     for file in files:
-        to_json_parser = convert_to_json_factory(format, file)
+        try:
+            to_json_parser = convert_to_json_factory(format, file)
+        except ValueError as error:
+            print(error)
+            continue
+
         to_json_parser.write_to_file(
             get_file_name_without_extension(file) + ".json"
         )
--- parsers/parser_factory.py
+++ parsers/parser_factory.py
@@ -7,13 +7,14 @@ import sys
 
 
 def convert_to_json_factory(format, file):
+    # Instaniate the correct parser
     if format == "xml":
         file_name = get_file_name_from_path(file)
         try:
             root = ET.parse(file).getroot()
         except(ET.ParseError):
-            print(f"error: The file {file_name} is not correctly-formatted")
-            sys.exit(1)
+            raise ValueError(
+                f"error: The file {file_name} is not correctly-formatted")
         return XMLParser(root, file_name)
     if format == "csv":
         csv_data = open(file)
