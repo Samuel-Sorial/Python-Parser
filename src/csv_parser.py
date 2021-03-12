f0fed87a7aa9407dfdbca713825eb1695f8f07b1
Samuel-Sorial <eng.samuel.maged@gmail.com>
2021-03-12 22:40:33 +0200

    Completing csv parser class (wirte & parse methods)

--- src/csv_parser.py
+++ src/csv_parser.py
@@ -1,5 +1,6 @@
 import csv
 from utils import pascal_to_snake_case
+import json
 
 
 class CSVParser:
@@ -8,6 +9,12 @@ class CSVParser:
         self.__heading = heading
 
     def parse(self):
-        for line in self.__dict_reader:
+        self.__dict_reader.fieldnames = [pascal_to_snake_case(
+            field) for field in self.__dict_reader.fieldnames]
 
-            print(line.items())
+        return list(self.__dict_reader)
+
+    def write_to_file(self, file_name):
+        csv_list = self.parse()
+        with open(file_name, "w") as json_file:
+            json.dump(csv_list, json_file)
