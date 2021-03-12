c29116131c510fd258830856d7459ffdca3ccde4
Samuel-Sorial <eng.samuel.maged@gmail.com>
2021-03-12 16:40:25 +0200

    Testing nested xml with attributes

--- test/xml_parser_test.py
+++ test/xml_parser_test.py
@@ -21,7 +21,19 @@ class TestParser(unittest.TestCase):
         curr_dict = parser.parse()
         self.assertDictEqual(
             curr_dict, {"file_name": f"xml/{file_name}", "transaction":
-                        {"date": "18/1/1998", "name": {"first": "Samuel", "last": "Sorial"}}})
+                        {"date": "18/1/1998", "name": {"first": "Samuel",
+                                                       "last": "Sorial"}}})
+
+    def test_nested_with_attributes(self):
+        xml_tree = ET.fromstring(
+            '<?xml version="1.0" encoding="UTF-8"?><Transaction id="1234"> <Date>18/1/1998</Date> <Name> <First>Samuel</First><Last>Sorial</Last></Name> </Transaction>')
+        file_name = "transaction"
+        parser = XMLParser(xml_tree, file_name)
+        curr_dict = parser.parse()
+        self.assertDictEqual(
+            curr_dict, {"file_name": f"xml/{file_name}", "transaction":
+                        {"id": "1234", "date": "18/1/1998", "name": {"first":
+                                                                     "Samuel", "last": "Sorial"}}})
 
 
 if __name__ == '__main__':
