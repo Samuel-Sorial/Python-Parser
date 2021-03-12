a8833f29ef34aa5234c557f8b5d0d3ee7b54f33f
Samuel-Sorial <eng.samuel.maged@gmail.com>
2021-03-12 19:15:39 +0200

    Extracting the helper methods to the utils module

--- src/parser.py
+++ src/parser.py
@@ -1,46 +1,14 @@
-import argparse
-import sys
-import os
 from parser_factory import convert_to_json_factory
-
-
-def extract_args():
-    # Returns the command line arguments
-    parser = argparse.ArgumentParser(
-        description="Parse given files from a given format to JSON format")
-    parser.add_argument("format", choices=[
-        "csv", "xml"],
-        help="The format of the given file(s)")
-
-    parser.add_argument("files", nargs='+', help="File(s) to be parsed")
-    arguments = parser.parse_args(sys.argv[1:])
-    return arguments.format, arguments.files
-
-
-def validate_args(format, files):
-    # if any file doesn't exist or is not with given format terminate!!
-    errors = []
-    format_len = len(format)
-    for file in files:
-        if not os.path.exists(file):
-            errors.append(f"error: The file {file} doesn't exist")
-            continue
-
-        if file[-format_len:] != format:
-            errors.append(f"error: The file {file} is not a {format} file!!")
-
-    if len(errors) > 0:
-        print(*errors)
-        sys.exit(1)
+from utils import extract_args, validate_args
 
 
 def main():
     format, files = extract_args()
     validate_args(format, files)
-    # Parse each file
     for file in files:
         parser = convert_to_json_factory(format, file)
-        parser.parse()
+        parsed_dict = parser.parse()
+        print(parsed_dict)
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import re
-
+import argparse
+import sys
+import os
 
 # Compile the pattern to improve performance
 # Inspired by: https://stackoverflow.com/a/1176023/13089670
@@ -9,3 +11,33 @@ pattern = re.compile(r'(?<!^)(?=[A-Z])')
 def pascal_to_snake_case(text):
     text = text.strip()
     return pattern.sub('_', text).lower()
+
+
+def extract_args():
+    # Returns the command line arguments
+    parser = argparse.ArgumentParser(
+        description="Parse given files from a given format to JSON format")
+    parser.add_argument("format", choices=[
+        "csv", "xml"],
+        help="The format of the given file(s)")
+
+    parser.add_argument("files", nargs='+', help="File(s) to be parsed")
+    arguments = parser.parse_args(sys.argv[1:])
+    return arguments.format, arguments.files
+
+
+def validate_args(format, files):
+    # if any file doesn't exist or is not with given format terminate!!
+    errors = []
+    format_len = len(format)
+    for file in files:
+        if not os.path.exists(file):
+            errors.append(f"error: The file {file} doesn't exist")
+            continue
+
+        if file[-format_len:] != format:
+            errors.append(f"error: The file {file} is not a {format} file!!")
+
+    if len(errors) > 0:
+        print(*errors)
+        sys.exit(1)
