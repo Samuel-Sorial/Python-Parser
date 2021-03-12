8994c130ab3e1ca3b937465302557b99abf7e688
Samuel-Sorial <eng.samuel.maged@gmail.com>
2021-03-12 17:02:55 +0200

    Removing path from the xml heading

--- src/xml_parser.py
+++ src/xml_parser.py
@@ -4,7 +4,8 @@ from collections import defaultdict
 class XMLParser:
     def __init__(self, xml_root, heading):
         self.__xml_root = xml_root
-        self.__heading = heading
+        file_name = heading[heading.rfind('/')+1:]
+        self.__heading = file_name
 
     def __append_to_neighbours(self, neighbours_dict, member_dict):
         for key, value in member_dict.items():
