deaca5ef52c3fec2ea8b283e1ea93c18174fa22a
Samuel-Sorial <eng.samuel.maged@gmail.com>
2021-03-12 19:26:22 +0200

    Adding more tests for get file without extensions

--- test/utils_test.py
+++ test/utils_test.py
@@ -5,18 +5,29 @@ import unittest
 class PascalToSnakeCaseTest(unittest.TestCase):
     def test_without_space(self):
         snake = pascal_to_snake_case("SamuelSorial")
+
         self.assertEqual(snake, "samuel_sorial")
 
     def test_with_space(self):
         snake = pascal_to_snake_case(" SamuelSorial ")
+
         self.assertEqual(snake, "samuel_sorial")
 
 
 class GetFileNameWithoutExtension(unittest.TestCase):
-    def test_with_path(self):
+    def test_without_path(self):
         first_file = get_file_name_without_extension("samuel.xml")
         second_file = get_file_name_without_extension("samuel.csv")
         third_file = get_file_name_without_extension("Samuel.csv")
+
+        self.assertEqual(first_file, "samuel")
+        self.assertEqual(second_file, "samuel")
+        self.assertEqual(third_file, "Samuel")
+
+    def test_with_path(self):
+        first_file = get_file_name_without_extension("./data/xml/samuel.xml")
+        second_file = get_file_name_without_extension("./data/csv/samuel.csv")
+        third_file = get_file_name_without_extension("./data/csv/Samuel.csv")
 
         self.assertEqual(first_file, "samuel")
         self.assertEqual(second_file, "samuel")
