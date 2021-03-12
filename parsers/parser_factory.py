59576a26fb708728e48477150d98bbff0035518a
Samuel-Sorial <eng.samuel.maged@gmail.com>
2021-03-12 23:42:57 +0200

    Handling incorrect xml formatted files

--- parsers/parser_factory.py
+++ parsers/parser_factory.py
@@ -3,12 +3,18 @@ from .csv_parser import CSVParser
 from .utils import get_file_name_from_path
 import xml.etree.ElementTree as ET
 import csv
+import sys
 
 
 def convert_to_json_factory(format, file):
     if format == "xml":
-        return XMLParser(ET.parse(file).getroot(),
-                         get_file_name_from_path(file))
+        file_name = get_file_name_from_path(file)
+        try:
+            root = ET.parse(file).getroot()
+        except(ET.ParseError):
+            print(f"error: The file {file_name} is not correctly-formatted")
+            sys.exit(1)
+        return XMLParser(root, file_name)
     if format == "csv":
         csv_data = open(file)
         return CSVParser(csv.DictReader(csv_data),
