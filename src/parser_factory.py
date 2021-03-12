f1e3e0bfbb5b3793ea63dc1c13ac3e8c8603b2c3
Samuel-Sorial <eng.samuel.maged@gmail.com>
2021-03-12 02:49:12 +0200

    Completing parse xml to dictinoary algorithm

--- src/parser_factory.py
+++ src/parser_factory.py
@@ -10,30 +10,34 @@ class XMLToJson:
         self.file = file
 
     def __append_to_neighbours(self, neighbours_dict, member_dict):
-        # TODO
-        pass
+        for key, value in member_dict.items():
+            neighbours_dict[key.lower()].append(value)
 
     def __flatten_children(self, child_dict, parent_dict):
-        # TODO
-        pass
+        for key, value in child_dict.items():
+            parent_dict[key] = value[0] if len(value) == 1 else value
 
     def __add_attributes(self, tree_dict, tree):
-        # TODO
-        pass
+        for key, value in tree.attrib.items():
+            tree_dict[key.lower()] = value
 
     def __convert_tree_to_dict(self, tree):
+        # Parse XML tree to dict[string : dict || list]
+        # Inspired from: https://stackoverflow.com/a/10076823/13089670
         children_dect = defaultdict(list)
         for child in tree.getchildren():
             current_child_dict = self.__convert_tree_to_dict(child)
             self.__append_to_neighbours(children_dect, current_child_dict)
 
-        xml_dict = {tree.tag: {}}
-        self.__flatten_children(children_dect, xml_dict[tree.tag])
-        self.__add_attributes(xml_dict[tree.tag], tree)
+        tag = tree.tag.lower()
+        xml_dict = {tag: {}}
+        self.__flatten_children(children_dect, xml_dict[tag])
+        self.__add_attributes(xml_dict[tag], tree)
 
         if tree.text.strip():
             text = tree.text.strip()
             xml_dict[tree.tag] = text
+
         return xml_dict
 
     def parse(self):
